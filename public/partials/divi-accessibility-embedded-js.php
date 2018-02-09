3ac1525dc3dc3bca82c4a6b6352b004feb65bbd4
Joseph Fusco <hello@josephfus.co>
2018-02-08 22:04:30 -0500

    Prevent spacebar from scrolling page for toggles

--- public/partials/divi-accessibility-embedded-js.php
+++ public/partials/divi-accessibility-embedded-js.php
@@ -226,10 +226,26 @@ if ( $this->can_load( 'focusable_modules' ) ) { ?>
 				$(this).attr('tabindex', 0);
 			});
 
-			$(document).keyup(function (e) {
-				// Enter.
-				if (e.which === 13) {
-					// Expand Accordion & Toggle modules when enter is hit while focused.
+			/**
+			 * Prevent spacebar from scolling page when toggle & accordion have focus.
+			 *
+			 * @divi-module  Accordion, Toggle
+			 */
+			$('.et_pb_toggle').on('keydown', function(e) {
+				// Spacebar.
+				if (e.which === 32){
+					e.preventDefault();
+				}
+			});
+
+			/**
+			 * Expand Accordion & Toggle modules when enter or spacebar are pressed while focused.
+			 *
+			 * @divi-module  Accordion, Toggle
+			 */
+			$(document).on('keyup', function(e) {
+				// Spacebar & Enter.
+				if (e.which === 13 || e.which === 32) {
 					$('.et_pb_toggle:focus .et_pb_toggle_title').trigger('click');
 				}
 			});
