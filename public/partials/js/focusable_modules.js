c16473d5f7a72a5c749a71dbe5779504a3f665a3
RichardGeorgeDavis <richard@lucidity.design>
2026-04-09 09:20:15 +0200

    Fix toggle button semantics

--- public/partials/js/focusable_modules.js
+++ public/partials/js/focusable_modules.js
@@ -1,54 +1,89 @@
 jQuery(document).ready(function($) {
+	var toggleButtonClass = 'da11y-toggle-trigger';
+	var toggleButtonStyleId = 'da11y-focusable-modules-style';
+	var toggleSyncDelay = 500;
+
+	/**
+	 * Add button reset styles for generated toggle controls.
+	 *
+	 * @divi-module Accordion, Toggle
+	 */
+	function ensureToggleButtonStyles() {
+		if ($('#' + toggleButtonStyleId).length) {
+			return;
+		}
+
+		$('<style />', {
+			id: toggleButtonStyleId,
+			text: '.et_pb_toggle_title .' + toggleButtonClass + '{-webkit-appearance:none;appearance:none;background:none;border:0;border-radius:0;box-shadow:none;color:inherit;cursor:pointer;display:block;font:inherit;line-height:inherit;margin:0;padding:0;text-align:inherit;width:100%;}'
+		}).appendTo('head');
+	}
+
+	/**
+	 * Get a generated button from the current toggle title.
+	 *
+	 * @divi-module Accordion, Toggle
+	 *
+	 * @param {jQuery} $toggle
+	 * @returns {jQuery}
+	 */
+	function getToggleButton($toggle) {
+		return $toggle.find('.et_pb_toggle_title > .' + toggleButtonClass).first();
+	}
+
+	/**
+	 * Sync aria state to the generated toggle button.
+	 *
+	 * @divi-module Accordion, Toggle
+	 *
+	 * @param {jQuery} $toggle
+	 */
+	function syncToggleButtonState($toggle) {
+		var $panel = $toggle.find('.et_pb_toggle_content').first();
+		var $button = getToggleButton($toggle);
+		var isAccordion = $toggle.hasClass('et_pb_accordion_item');
+		var isOpen = $toggle.hasClass('et_pb_toggle_open');
+
+		if (!$panel.length || !$button.length) {
+			return;
+		}
+
+		$button.attr({
+			'aria-controls': $panel.attr('id'),
+			'aria-expanded': isOpen ? 'true' : 'false',
+		});
+
+		if (isAccordion && isOpen) {
+			$button.attr('aria-disabled', 'true');
+		} else {
+			$button.removeAttr('aria-disabled');
+		}
+	}
 
 	/**
 	 * Add appropriate aria attributes to Accordion & Toggle Divi modules
 	 *
 	 * @divi-module  Accordion, Toggle
 	 */
+	ensureToggleButtonStyles();
+
 	$('.et_pb_toggle').each(function (index) {
 		var $toggle = $(this);
 		var $title = $toggle.find('.et_pb_toggle_title');
 		var $panel = $toggle.find('.et_pb_toggle_content');
-		var isAccordion = $toggle.hasClass('et_pb_accordion_item');
+		var $button = getToggleButton($toggle);
 
-		$title.attr('role', 'button');
-		$title.attr('tabindex', 0);
-		$title.attr('aria-controls', 'et_pb_toggle_content_' + index);
-		$panel.attr('id', 'et_pb_toggle_content_' + index);
-
-		if ($toggle.hasClass('et_pb_toggle_open')) {
-			$title.attr('aria-expanded', true);
-
-			if (isAccordion) $title.attr('aria-disabled', true);
-		} else {
-			$title.attr('aria-expanded', false);
-
-			if (isAccordion) $title.removeAttr('aria-disabled');
+		if (!$panel.attr('id')) {
+			$panel.attr('id', 'et_pb_toggle_content_' + index);
 		}
-	});
 
-	/**
-	 * Prevent spacebar from scolling page when toggle & accordion have focus.
-	 *
-	 * @divi-module  Accordion, Toggle
-	 */
-	$('.et_pb_toggle_title').on('keydown', function(e) {
-		// Spacebar.
-		if (e.which === 32){
-			e.preventDefault();
+		if (!$button.length) {
+			$title.wrapInner('<button type="button" class="' + toggleButtonClass + '"></button>');
+			$button = getToggleButton($toggle);
 		}
-	});
 
-	/**
-	 * Expand Accordion & Toggle modules when enter or spacebar are pressed while focused.
-	 *
-	 * @divi-module  Accordion, Toggle
-	 */
-	$(document).on('keyup', function(e) {
-		// Spacebar & Enter.
-		if (e.which === 13 || e.which === 32) {
-			$('.et_pb_toggle_title:focus').trigger('click');
-		}
+		$button.attr('type', 'button');
+		syncToggleButtonState($toggle);
 	});
 
 	/**
@@ -57,41 +92,18 @@ jQuery(document).ready(function($) {
 	 * @divi-module  Accordion, Toggle
 	 */
 	$('.et_pb_toggle_title').on('click', function() {
-		var $clickedToggleTitle = $(this);
-		var $clickedToggle = $clickedToggleTitle.parent();
-		var isAccordion = $clickedToggle.hasClass('et_pb_accordion_item');
+		var $clickedToggle = $(this).closest('.et_pb_toggle');
+		var $togglesToSync = $clickedToggle;
 
-		if (isAccordion) {
-			// Only change the aria attributes if an accordion toggle isn't already open
-			if (!$clickedToggle.hasClass('et_pb_toggle_open')) {
-				var $allSiblingToggles = $clickedToggleTitle.closest('.et_pb_accordion').find('.et_pb_toggle');
-
-				// Reset the aria attributes on the open toggle
-				$allSiblingToggles.each(function() {
-					$toggle = $(this);
-					if ($toggle.hasClass('et_pb_toggle_open')) {
-						var $openToggleTitle = $toggle.find('.et_pb_toggle_title')
-
-						$openToggleTitle.attr('aria-expanded', false);
-						$openToggleTitle.removeAttr('aria-disabled');
-					}
-				});
-
-				// Set the aria attributes on the clicked toggle
-				setTimeout(function() {
-					$clickedToggleTitle.attr('aria-expanded', true);
-					$clickedToggleTitle.attr('aria-disabled', true);
-				}, 500);
-			}
-		} else {
-			// The toggle isn't part of an accordion so its aria attributes should be udpated
-			if ($clickedToggle.hasClass('et_pb_toggle_open')) {
-				$clickedToggleTitle.attr('aria-expanded', false);
-			} else {
-				$clickedToggleTitle.attr('aria-expanded', true);
-			}
+		if ($clickedToggle.hasClass('et_pb_accordion_item')) {
+			$togglesToSync = $clickedToggle.closest('.et_pb_accordion').find('.et_pb_toggle');
 		}
+
+		setTimeout(function() {
+			$togglesToSync.each(function() {
+				syncToggleButtonState($(this));
+			});
+		}, toggleSyncDelay);
 	});
 
 });
-
